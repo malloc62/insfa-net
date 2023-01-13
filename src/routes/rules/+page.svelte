634faf191fd79353299299c7ebee1696ad811f51
malloc62 <xodrium@gmail.com>
2023-01-12 22:40:00 -0500

    Rule update

--- src/routes/rules/+page.svelte
+++ src/routes/rules/+page.svelte
@@ -8,7 +8,7 @@
 <Section>
     <h1>Global Discord Rules</h1>
     <p>
-        1. Display common sense: do not excessively harass other members of the community, or participate in immoral activity.
+        1. Display common sense: do not excessively harass other members of the community, be a nusiance, or participate in immoral activity.
     </p>
     <p>
         1a. Respect others regardless of race, sexual orientation, gender, or other factors. 
@@ -16,8 +16,10 @@
     <p>
         2. Advertising is accepted, as long as it isn't the sole purpose of you joining this community.
     </p>
-        3. No excessive NSFW or suggestive remarks, especially if done to the point that server members get uncomfortable with the situation.
+    <p>
+        3. No excessive NSFW, suggestive remarks, or sensitive jokes, especially if done to the point that server members get uncomfortable with the situation.
+    </p.
     <p>
         4. Use channels for their dedicated purpose: this includes isolating spam and bot commands to only the #spam channel.
-    </p>        
+    </p> 
 </Section>

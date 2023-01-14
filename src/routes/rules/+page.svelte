a5e17ed65704fa0526f05ec4ad2588699a76d2a3
malloc62 <xodrium@gmail.com>
2023-01-13 22:44:05 -0500

    Expanded rules document

--- src/routes/rules/+page.svelte
+++ src/routes/rules/+page.svelte
@@ -23,3 +23,45 @@
         4. Use channels for their dedicated purpose: this includes isolating spam and bot commands to only the #spam channel.
     </p> 
 </Section>
+<Section>
+    <h1>Global Terms of Service</h1>
+    <p>
+        1. Do not attempt to take down or hack any platforms owned under Insfa.net.
+    </p>
+    <p>
+        2. Do not attempt to evade any punishments given.
+    </p>
+</Section>
+<Section>
+    <h1>PlanarCraph Rules</h1>
+    <p>
+        1. You may not grief land that is owned by a nation that isn't yours, nor can you severely grief unclaimed land to the point of making it unusable.
+    </p>
+    <p>
+        2. Apart from minimap mods configured to only show the surface, shader mods, FPS mods, and certain clients (Lunar Client and Badlion Client), modifications are banned.
+    </p>
+    <p>
+        3. All claims, wars, and peace treaties must be handled under the #declarations forum channel to be confirmed or denied by moderators, and to be ratiifed officially.
+    </p>
+    <p>
+        4. If you claim too much land, you'll be asked to reduce it before a moderator approves. For a sense of scale, look at other claims in #claims. Take note that larger population, development, potential popularity of the region, nearby claims, and conquest via war are all factored into how much you can claim.
+    </p>
+    <p>
+        5. First person to claim land, gets it. Land can be joint owned, but is conquered as if it was owned by one nation.
+    </p>
+</Section>
+<Section>
+    <h1>PlanarCraph War Rules</h1>
+    <p>
+        1. A war declaration is only valid upon pinging the opposing leader(s).
+    </p>
+    <p>
+        2. Nations that have existed for 3 days or shorter are immune to war declarations, unless the state in question declares war on another entity.
+    </p>
+    <p>
+        3. All grief rules are void in any nations involved in a war.
+    </p>
+    <p>
+        4. If the opposition is significantly griefed, a nation can write a peace treaty. Any reasonably enforcable conditions that are broken in a treaty will void any war immunity the opposition may have.
+    </p>
+</Section>
